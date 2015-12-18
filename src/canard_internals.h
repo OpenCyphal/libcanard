6091d174e67e59a39ddd99024f514cf389c6b956
masierra <masierra@ucsc.edu>
2015-12-17 17:39:02 -0800

    [libcanard] cleanup

--- src/canard_internals.h
+++ src/canard_internals.h
@@ -17,16 +17,17 @@ CANARD_INTERNAL CanardRxState *canardRxStateTraversal(CanardInstance* ins, const
 CANARD_INTERNAL CanardRxState *canardCreateRxState(CanardPoolAllocator* allocator, uint32_t transfer_descriptor);
 CANARD_INTERNAL CanardRxState *canardAppendRxState(CanardInstance* ins, uint32_t transfer_descriptor);
 CANARD_INTERNAL CanardRxState *canardFindRxState(CanardRxState* state, uint32_t transfer_descriptor);
-CANARD_INTERNAL void canardPrintStates(CanardInstance* ins);
 CANARD_INTERNAL void canardBufferBlockPushBytes(CanardPoolAllocator* allocator, CanardRxState* state, const uint8_t* data, uint8_t data_len);
 CANARD_INTERNAL CanardBufferBlock *canardCreateBufferBlock(CanardPoolAllocator* allocator);
 CANARD_INTERNAL uint8_t canardTransferType(uint32_t id);
 CANARD_INTERNAL uint16_t canardDataType(uint32_t id);
 CANARD_INTERNAL void canardPushTxQueue(CanardInstance* ins, CanardTxQueueItem* item);
 CANARD_INTERNAL bool priorityHigherThan(uint32_t id, uint32_t rhs);
-CANARD_INTERNAL void canardPrintQueue(CanardInstance* ins);
 CANARD_INTERNAL CanardTxQueueItem *canardCreateTxItem(CanardPoolAllocator* allocator);
-CANARD_INTERNAL void cleanCanardRxState(CanardRxState* state);
+CANARD_INTERNAL void canardPrepareForNextTransfer(CanardRxState* state);
+CANARD_INTERNAL int computeForwardDistance(uint8_t a, uint8_t b);
+CANARD_INTERNAL void tidIncrement(uint8_t* transfer_id);
+CANARD_INTERNAL uint64_t canardReleaseStatePayload(CanardInstance* ins, CanardRxState* rxstate);
 
 /** Inits a memory allocator.
  *
