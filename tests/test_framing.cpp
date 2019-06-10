1b4e36964921fc162e54791282b8ceb84fd4b53d
Kjetil Kjeka <kjetilkjeka@gmail.com>
2019-06-10 15:13:36 +0200

    Add test for scenarios where CRC frame is sent in a seperate CAN frame and split between CAN frames

--- tests/test_framing.cpp
+++ tests/test_framing.cpp
@@ -233,3 +233,147 @@ TEST_CASE("Framing, MultiFrameBasicCan2")
     REQUIRE(canardPeekTxQueue(&ins) == nullptr);               
 }
 
+/* This test is created to see that sending the CRC in a sepereate frame works fine.
+ * When all the data is sent with no free byte for CRC in the last frame, 
+ * the CRC bytes must be sent in a seperate frame.
+ */
+TEST_CASE("Framing, MultiFrameSeperateCRCCan2")
+{
+    uint8_t node_id = 22;
+    uint8_t toggle_bit = 5;
+    uint8_t eof_bit = 6;
+    uint8_t sof_bit = 7;
+
+    std::uint8_t memory_arena[4096];
+    ::CanardInstance ins;
+
+    uint16_t subject_id = 12;
+    uint8_t transfer_id = 2;
+    uint8_t transfer_priority = CANARD_TRANSFER_PRIORITY_NOMINAL;
+    uint8_t data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    
+    canardInit(&ins,
+               memory_arena,
+               sizeof(memory_arena),
+               onTransferReceptionMock,
+               acceptAllTransfers,
+               reinterpret_cast<void*>(12345));
+    canardSetLocalNodeID(&ins, node_id);
+
+    auto res = canardPublishMessage(&ins,
+                         subject_id,
+                         &transfer_id,
+                         transfer_priority,
+                         data,
+                         sizeof(data));
+    REQUIRE(res >= 0);
+
+    // compenaste for internally incrementing transfer_id in canardPublishMessage
+    transfer_id--;
+
+
+    // First frame (1)
+    auto transfer_frame = canardPeekTxQueue(&ins);
+    REQUIRE(transfer_frame != nullptr);
+    canardPopTxQueue(&ins);
+
+    REQUIRE(transfer_frame->data_len == 8);
+    // Data correctness is to be checked in unrelated test                
+    REQUIRE(transfer_frame->data[7] == ((1 << sof_bit) | (0 << eof_bit) | (1 << toggle_bit) | transfer_id));
+
+
+    // Second frame (2) - Contains the ramining of the data
+    transfer_frame = canardPeekTxQueue(&ins);
+    REQUIRE(transfer_frame != nullptr);
+    canardPopTxQueue(&ins);
+    REQUIRE(transfer_frame->data_len == 8);
+    // Data correctness is to be checked in unrelated test   
+    REQUIRE(transfer_frame->data[7] == ((0 << sof_bit) | (0 << eof_bit) | (0 << toggle_bit) | transfer_id));
+
+
+    // Third and last frame (3) - Only contains the last CRC byte and tail byte
+    transfer_frame = canardPeekTxQueue(&ins);
+    REQUIRE(transfer_frame != nullptr);
+    canardPopTxQueue(&ins);
+
+    REQUIRE(transfer_frame->data_len == 3);
+    // CRC correctness is to be checked in unrelated test                                 
+    REQUIRE(transfer_frame->data[2] == ((0 << sof_bit) | (1 << eof_bit) | (1 << toggle_bit) | transfer_id));
+
+
+    // Make sure there are no frames after the last frame
+    REQUIRE(canardPeekTxQueue(&ins) == nullptr);               
+}
+
+/* This test is created to see that split CRC between frames works fine.
+ * When all the data is sent with only one free byte for CRC, 
+ * the last CRC byte must be sent in a seperate frame.
+ */
+TEST_CASE("Framing, MultiFrameSplitCRCCan2")
+{
+    uint8_t node_id = 22;
+    uint8_t toggle_bit = 5;
+    uint8_t eof_bit = 6;
+    uint8_t sof_bit = 7;
+
+    std::uint8_t memory_arena[4096];
+    ::CanardInstance ins;
+
+    uint16_t subject_id = 12;
+    uint8_t transfer_id = 2;
+    uint8_t transfer_priority = CANARD_TRANSFER_PRIORITY_NOMINAL;
+    uint8_t data[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    
+    canardInit(&ins,
+               memory_arena,
+               sizeof(memory_arena),
+               onTransferReceptionMock,
+               acceptAllTransfers,
+               reinterpret_cast<void*>(12345));
+    canardSetLocalNodeID(&ins, node_id);
+
+    auto res = canardPublishMessage(&ins,
+                         subject_id,
+                         &transfer_id,
+                         transfer_priority,
+                         data,
+                         sizeof(data));
+    REQUIRE(res >= 0);
+
+    // compenaste for internally incrementing transfer_id in canardPublishMessage
+    transfer_id--;
+
+
+    // First frame (1)
+    auto transfer_frame = canardPeekTxQueue(&ins);
+    REQUIRE(transfer_frame != nullptr);
+    canardPopTxQueue(&ins);
+
+    REQUIRE(transfer_frame->data_len == 8);
+    // Data correctness is to be checked in unrelated test                
+    REQUIRE(transfer_frame->data[7] == ((1 << sof_bit) | (0 << eof_bit) | (1 << toggle_bit) | transfer_id));
+
+
+    // Second frame (2) - Contains the first CRC byte
+    transfer_frame = canardPeekTxQueue(&ins);
+    REQUIRE(transfer_frame != nullptr);
+    canardPopTxQueue(&ins);
+    REQUIRE(transfer_frame->data_len == 8);
+    // Data correctness is to be checked in unrelated test   
+    REQUIRE(transfer_frame->data[7] == ((0 << sof_bit) | (0 << eof_bit) | (0 << toggle_bit) | transfer_id));
+
+
+    // Third and last frame (3) - Only contains the last CRC byte and tail byte
+    transfer_frame = canardPeekTxQueue(&ins);
+    REQUIRE(transfer_frame != nullptr);
+    canardPopTxQueue(&ins);
+
+    REQUIRE(transfer_frame->data_len == 2);
+    // CRC correctness is to be checked in unrelated test                                 
+    REQUIRE(transfer_frame->data[1] == ((0 << sof_bit) | (1 << eof_bit) | (1 << toggle_bit) | transfer_id));
+
+
+    // Make sure there are no frames after the last frame
+    REQUIRE(canardPeekTxQueue(&ins) == nullptr);               
+}
+
