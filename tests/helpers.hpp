5c8acd1fa9352c32cd20e808279bff9f519e13c2
joshvazquez-amzn <joshvazq@amazon.com>
2022-10-13 13:08:18 -0700

    Fix formatting

--- tests/helpers.hpp
+++ tests/helpers.hpp
@@ -272,8 +272,7 @@ public:
         const auto size_before = que_.size;
         const auto ret         = canardTxPush(&que_, ins, transmission_deadline_usec, &metadata, payload_size, payload);
         const auto num_added   = static_cast<std::size_t>(ret);
-        enforce((ret < 0) || ((size_before + num_added) == que_.size),
-                "Unexpected size change after push");
+        enforce((ret < 0) || ((size_before + num_added) == que_.size), "Unexpected size change after push");
         checkInvariants();
         return ret;
     }
