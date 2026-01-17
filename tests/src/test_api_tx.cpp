3d07808075eda2c12a6a66008fc31af280d88ef5
Pavel Kirienko <pavel.kirienko@gmail.com>
2026-01-17 18:14:11 +0200

    test stub

---
+++ tests/src/test_api_tx.cpp
@@ -0,0 +1,20 @@
+// This software is distributed under the terms of the MIT License.
+// Copyright (c) OpenCyphal Development Team.
+
+#include <canard.h>
+#include <unity.h>
+#include <cstdint>
+#include <cstdlib>
+#include <memory>
+
+extern "C" void setUp() {}
+extern "C" void tearDown() {}
+
+int main()
+{
+    UNITY_BEGIN();
+
+    // TODO add tests
+
+    return UNITY_END();
+}
